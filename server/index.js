aafbd04da33a0da5817ae482f70b4839368451df
Divyansh Garg <garg.divyansh20@gmail.com>
2022-07-17 12:16:03 +0530

    commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+
+app.use(bodyParser.json({limit:"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const connection_url = 'mongodb+srv://divyansh_20:divyansh_20@cluster0.yaymr.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(connection_url,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err)=> console.log(err));
+
